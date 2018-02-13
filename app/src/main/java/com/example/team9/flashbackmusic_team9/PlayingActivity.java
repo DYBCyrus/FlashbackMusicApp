01c5531c6d5a1517e482cf2db6fe190d985ed6da
rdpatel26 <rdp2606@gmail.com>
2018-02-13 09:34:35 -0800

    play_pause functionality on PlayingActivity

--- app/src/main/java/com/example/team9/flashbackmusic_team9/PlayingActivity.java
+++ app/src/main/java/com/example/team9/flashbackmusic_team9/PlayingActivity.java
@@ -9,6 +9,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -17,15 +18,17 @@ import android.widget.TextView;
 
 
 public class PlayingActivity extends AppCompatActivity {
-    @Override
 
+
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playing);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        Button back = (Button)findViewById(R.id.back);
+        Button back = findViewById(R.id.back);
         back.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -35,22 +38,35 @@ public class PlayingActivity extends AppCompatActivity {
             }
         });
 
-        TextView title = (TextView)findViewById(R.id.title);
-        TextView artist = (TextView)findViewById(R.id.artist);
-        TextView album = (TextView)findViewById(R.id.album);
-        TextView location = (TextView)findViewById(R.id.location);
-        TextView time = (TextView)findViewById(R.id.time);
+
+        final ImageButton fav = findViewById(R.id.likeButton);
+        final ImageButton play_pause = findViewById(R.id.play_pause_button);
+
+        TextView title = findViewById(R.id.title);
+        TextView artist = findViewById(R.id.artist);
+        TextView album = findViewById(R.id.album);
+        TextView location = findViewById(R.id.location);
+        TextView time = findViewById(R.id.time);
 
         title.setText(Player.getCurrentTrack().getName());
         artist.setText(Player.getCurrentTrack().getArtist());
         album.setText(Player.getCurrentTrack().getAlbum().getName());
 
-        final ImageButton fav = (ImageButton) findViewById(R.id.likeButton);
+        // Checking track status before launching activity for like_dislike button image
+        if( Player.getCurrentTrack().getStatus() == Track.FavoriteStatus.DISLIKE ){
+            fav.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.x, null));
+        }
+        else if( Player.getCurrentTrack().getStatus() == Track.FavoriteStatus.LIKE ){
+            fav.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.check_mark, null));
+        }
+        else{
+            fav.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.plus, null));
+        }
+
 
         fav.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 if( Player.getCurrentTrack().getStatus() == Track.FavoriteStatus.DISLIKE ){
                     Player.getCurrentTrack().setStatus(Track.FavoriteStatus.NEUTRAL);
                     fav.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.plus, null));
@@ -66,14 +82,20 @@ public class PlayingActivity extends AppCompatActivity {
             }
         });
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        play_pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if( Player.isPlaying() ){
+                    Player.pause();
+                    play_pause.setImageResource(R.drawable.play);
+                }
+                else{
+                    Player.resume();
+                    play_pause.setImageResource(R.drawable.pause);
+                }
+            }
+        });
     }
 
 }
