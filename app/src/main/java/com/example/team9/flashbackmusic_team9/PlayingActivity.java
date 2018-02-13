6b95bce9197c996be5b9cdb5ac2c1109316eee4f
rdpatel26 <rdp2606@gmail.com>
2018-02-13 08:40:56 -0800

    like, dislike, and neutral button

--- app/src/main/java/com/example/team9/flashbackmusic_team9/PlayingActivity.java
+++ app/src/main/java/com/example/team9/flashbackmusic_team9/PlayingActivity.java
@@ -1,13 +1,18 @@
 package com.example.team9.flashbackmusic_team9;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
@@ -40,14 +45,35 @@ public class PlayingActivity extends AppCompatActivity {
         artist.setText(Player.getCurrentTrack().getArtist());
         album.setText(Player.getCurrentTrack().getAlbum().getName());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        final ImageButton fav = (ImageButton) findViewById(R.id.likeButton);
+
+        fav.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                if( Player.getCurrentTrack().getStatus() == Track.FavoriteStatus.DISLIKE ){
+                    Player.getCurrentTrack().setStatus(Track.FavoriteStatus.NEUTRAL);
+                    fav.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.plus, null));
+                }
+                else if( Player.getCurrentTrack().getStatus() == Track.FavoriteStatus.LIKE ){
+                    Player.getCurrentTrack().setStatus(Track.FavoriteStatus.DISLIKE);
+                    fav.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.x, null));
+                }
+                else{
+                    Player.getCurrentTrack().setStatus(Track.FavoriteStatus.LIKE);
+                    fav.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.check_mark, null));
+                }
             }
         });
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
 }
