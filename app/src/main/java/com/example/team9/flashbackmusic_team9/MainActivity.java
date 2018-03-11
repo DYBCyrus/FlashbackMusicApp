13d1127ff3c3dc612aefa7881f224f78287c662e
Pranav Seshadri <pranavseshadri@gmail.co>
2018-03-11 14:19:43 -0700

    Dropdown menu
    Added the design for the dropdown menu not the functionality

--- app/src/main/java/com/example/team9/flashbackmusic_team9/MainActivity.java
+++ app/src/main/java/com/example/team9/flashbackmusic_team9/MainActivity.java
@@ -19,10 +19,12 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -38,9 +40,14 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.logging.Logger;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
+
+    //Dropdown menu and its options
+    private Spinner spinner;
+    private static final String[]paths = {"title","album","artist","favorite status"}
 
     private LocationManager locationManager;
     private static Location mLocation;
@@ -53,6 +60,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Creates DropDown menu
+        spinner = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, paths);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -173,6 +188,29 @@ public class MainActivity extends AppCompatActivity {
                 launchModeActivity();
             }
         });
+    }
+
+    //Function that takes care of the dropdown menu functionality
+    public void onItemSelected(AdapterView<?>parent, View v, int position, long id){
+
+        switch(position){
+            case 0:
+                //Implement for title
+                break;
+
+            case 1:
+                //Implement for album
+                break;
+
+            case 2:
+                //Implement for artist
+                break;
+
+            case 3:
+                //Implement for favorite status
+                break;
+
+        }
     }
 
     /**
